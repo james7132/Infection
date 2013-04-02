c4d36dffde6e753b1f8aa88a0e7946b2591670ac
Sebastian Monroy <sebastian.monroy@gmail.com>
2013-04-02 00:16:49 -0400

    Enemy Type Spawns Dependent on Score
    The probability of spawning each type of enemy is now done according to
    three offset sinusoids, one for each type of enemy. All of the sinusoids
    are dependent on the player's score, have the same frequency, and range
    in value from 0 to 9. Their timings are offset by 2pi/3.
    
    The first sinusoid, the dumb enemy sinusoid, defines the max number of
    dumb enemies allowed on the screen at this time. After 1/3 of the period
    of the dumb enemy sinusoid has elapsed, the smart enemy sinusoid begins,
    and so on for the smarter enemy. The type of enemy spawned is chosen by
    picking a random float between 0 and the sum of the values of the three
    sinusoids. If the random float is less than the value of the dumb
    sinusoid, a dumb enemy is spawned. Otherwise, if the random float is
    less than the value of the smart sinusoid, a smart enemy is spawned,
    etc, etc.
    
    I'm not sure if I've explained that well enough, so if it doesn't make
    sense check out the code I guess.
    
    The cool-down timer for the enemy span reduces by 0.02% of its current
    value every time an enemy is spawned. This is so that later in the game
    if you kill an enemy another will quickly spawn to take its place. I am
    considering other ways to ramp up the difficulty using the sinusoids
    (like reducing their frequencies, changing their offsets, and increasing
    their amplitudes over time), but for now I've decided to leave it in
    this basic form.
    
    STILL NEED TO SCALE UP ENEMY PARAMETERS (SPEED, FIRERATE, ETC.) WITH
    PLAYER'S SCORE.

--- Assets/Scripts/Enemy/WhiteBloodCellSpawner.cs
+++ Assets/Scripts/Enemy/WhiteBloodCellSpawner.cs
@@ -6,20 +6,46 @@ using System.Collections;
 public class WhiteBloodCellSpawner : MonoBehaviour {
 	public GameObject whiteBloodCell;
 	public float spawnTimer;
-	public float spawnTimerMax = 3.0f;
+	public float spawnTimerMax = 10.0f;
 	public const int upperLimit = 425;
 	public const int lowerLimit = -425;
 	public const int leftLimit = 525;
 	public const int rightLimit = -525;
+	private float score = 0;
+	public float enemyMax = 0;
+	public float dumbMax = 0;
+	public float smartMax = 0;
+	public float smarterMax = 0;
+	public static float totalConst = 9;
 	
 	// Update is called once per frame
 	void Update () {
-		if (spawnTimer<=0) {
-			// create the new white blood cell at randomly selected location.
-			spawnCell(getCellType(), getSpawnSide());
-			spawnTimer=spawnTimerMax;
+		score = ScoreScript.getScore();
+		float thing = score/400000.0f*2*Mathf.PI;
+		// spawn dumb white blood cells with increasing probability
+		dumbMax = totalConst/2.0f - Mathf.Cos(thing)*totalConst/2.0f;
+		// spawn smart white blood cells with increasing probability after dumb white blood cell spawn probability peaks
+		if (thing > 2.0f/3.0f*Mathf.PI) 	smartMax = totalConst/2.0f - Mathf.Cos(thing - 2.0f/3.0f*Mathf.PI)*totalConst/2.0f;
+		// spawn smarter white blood cells with increasing probability after rising smart white blood cell spawn probability has passed inflection point
+		if (thing > 4.0f/3.0f*Mathf.PI)		smarterMax = totalConst/2.0f + Mathf.Cos(thing + 2.0f/3.0f*Mathf.PI)*totalConst/2.0f;
+		// max total white blood cells is sum of max of each type of white blood cell
+		enemyMax = dumbMax + smartMax + smarterMax;
+		if (spawnTimer <= 0) {
+			// create the new white blood cell at randomly selected location
+			print (getWhiteCount ());
+			// only spawn anything if the white blood cell count is less than the max allowed
+			if (getWhiteCount() < enemyMax) {
+				print ("Dumb Max: " + dumbMax);
+				print ("Smart Max: " + smartMax);
+				print ("Smarter Max: " + smarterMax);
+				spawnCell(getCellType(), getSpawnSide());
+				spawnTimerMax *= 0.98f;
+			}
+			spawnTimer = spawnTimerMax;
+			// reduce the spawn wait by 0.2% each time an enemy is spawned;
+			print("spawnTimerMax = " + spawnTimerMax);
 		} else {
-			//print(spawnTimer);
+			// count down to spawn
 			spawnTimer-=Time.deltaTime;
 		}
 	
@@ -59,23 +85,25 @@ public class WhiteBloodCellSpawner : MonoBehaviour {
 	}
 	
 	private string getCellType () {
-		//double score = Global.getScore();
 		string enemyType;
-		int rand = Random.Range(0,3);
-		switch (rand) {
-		case 0:
+		// pick a number between 0 and the maximum number of enemies
+		float rand = Random.Range(0,enemyMax);
+		
+		if (rand <= dumbMax) {
+			print ("[ " + rand + " Dumb ]");
+			// spawn dumb enemy
 			enemyType = "Dumb";
-			break;
-		case 1:
+		} else if (rand <= dumbMax + smartMax) {
+			print("[ " + rand + " Smart ]");
+			// spawn smart enemy
 			enemyType = "Smart";
-			break;
-		case 2:
+		} else {
+			print("[ " + rand + " Smarter ]");
+			// spawn smarter enemy
 			enemyType = "Smarter";
-			break;
-		default:
-			goto case 0;
 		}
-		print (enemyType);
+		
+		//print (enemyType);
 		return enemyType;
 	}
 	
@@ -84,5 +112,12 @@ public class WhiteBloodCellSpawner : MonoBehaviour {
 		WhiteBloodCellUnit newUnit = newCell.GetComponent<WhiteBloodCellUnit>();
 		//print (newUnit);
 		newUnit.SendMessage("setType", enemyType);
+	}
+			
+	private int getWhiteCount() {
+		GameObject[] dumbs = GameObject.FindGameObjectsWithTag("Enemy Dumb");
+		GameObject[] smarts = GameObject.FindGameObjectsWithTag("Enemy Smart");
+		GameObject[] smarters = GameObject.FindGameObjectsWithTag("Enemy Smarter");
+		return dumbs.Length + smarts.Length + smarters.Length;
 	}
 }
